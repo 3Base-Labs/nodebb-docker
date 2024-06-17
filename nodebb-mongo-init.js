be191da6386fd9f874e9ac0681989c0ae46455e8
CaffeineDuck <hello@samrid.me>
2024-06-17 14:49:27 +0545

    feat: update workflows to work with 3base-labs

--- nodebb-mongo-init.js
+++
@@ -1,8 +0,0 @@
-db.createUser({
-  user: 'navystack-mongo',
-  pwd: 'oVvzozYfCgZXJ7373Y9VvmXh0J8WyF',
-  roles: [
-    { role: 'readWrite', db: 'navystack-mongo' },
-    { role: 'clusterMonitor', db: 'admin' }
-  ]
-})
